2b4cf95c60f4fb68597d128b76d62d8954776992
N Jayant Rao <njayantrao@gmail.com>
2026-03-10 17:53:11 +0530

    feat: Update server response messages and add health check endpoint

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,7 +3,11 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Server up n running... ✅✅");
+  res.status(200).json({ status: 200, message: "Server up n running... ✅✅" });
+});
+
+app.get("/health-check", (req, res) => {
+  res.status(200).json({ status: 200, message: "Server is healthy... ✅✅" });
 });
 
 export default app;
